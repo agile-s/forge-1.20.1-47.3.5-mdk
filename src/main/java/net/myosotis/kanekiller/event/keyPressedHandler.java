a1c273f852b6533eb5a37bd0265fccecb5261143
ohnomyhips <nik.icedorn08@gmail.com>
2024-08-11 22:36:44 +0300

    Fixed Bindings menu bug

--- src/main/java/net/myosotis/kanekiller/event/keyPressedHandler.java
+++ src/main/java/net/myosotis/kanekiller/event/keyPressedHandler.java
@@ -7,15 +7,19 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.myosotis.kanekiller.globalVars;
 import net.myosotis.kanekiller.util.keyboardHandler;
+import net.myosotis.kanekiller.world.Kanekiller;
 
 public class keyPressedHandler {
-    @Mod.EventBusSubscriber(modid = "kanekiller", bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = Kanekiller.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientEvents {
         @SubscribeEvent
         public static void keyRegistrator(RegisterKeyMappingsEvent event) {
             event.register(keyboardHandler.mykey);
         }
+    }
 
+    @Mod.EventBusSubscriber(modid = Kanekiller.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+    public static class onClick {
         @SubscribeEvent
         public static void onClientTick(TickEvent.ClientTickEvent event) {
             if (keyboardHandler.mykey.consumeClick()) {
